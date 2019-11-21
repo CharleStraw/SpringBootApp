367d85f4ac3a916674c6a385cb7375aab00f7b8a
karol <karussloma1@gmail.com>
2019-11-21 09:53:58 +0100

    21.11.2019

---
+++ src/main/java/com/app/Dao/StudentDAO.java
@@ -0,0 +1,49 @@
+package com.app.Dao;
+
+import com.app.Entity.Student;
+import org.springframework.stereotype.Repository;
+import java.util.Collection;
+import java.util.HashMap;
+
+
+//@Component is a generic stereotype for any Spring-managed component
+//@Service annotates classes at the service layer
+//@Repository annotates classes at the persistence layer, which will act as a database repository
+//@Controller is a common annotation which is used to mark a class as Spring MVC
+
+
+
+
+@Repository
+public class StudentDAO {
+
+
+    private static HashMap<Integer, Student> students;
+
+    static {
+
+        students = new HashMap<Integer, Student>() {
+
+            {
+
+                put(1, new Student(1, "Said", "Computer Science"));
+                put(2, new Student(2, "Karol", "Finance"));
+                put(3, new Student(3, "Mark", "Programing and Testing"));
+            }
+
+
+        };
+
+
+    }
+
+
+    public Collection<Student> getAllStudents() {
+
+        return this.students.values();
+
+
+    }
+
+
+}
--- src/main/java/com/app/Dao/studentDAO.java
+++
@@ -1,4 +0,0 @@
-package com.app.Dao;
-
-public class studentDAO {
-}
